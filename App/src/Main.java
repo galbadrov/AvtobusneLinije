576d0ed96eb1d6477f54930fa080bd605778c0d2
GalBadrov <gal.badrov@student.um.si>
2025-04-24 16:28:30 +0200

    implementiran prvi nacin izpisa

--- App/src/Main.java
+++ App/src/Main.java
@@ -15,21 +15,27 @@ public class Main {
         System.out.println("Vnesi stevilo naslednjih avtobusov:");
         //int st_avtobusov = sc.nextInt();
 
-        System.out.println("Vnesi nacin izpisa casa: \n 1- relative \n 2- absolute):");
+        System.out.println("Vnesi nacin izpisa casa: \n 1 - relative \n 2 - absolute):");
         //int nacin_zapisa = sc.nextInt();
 
-        //testni izpis
-        System.out.println("izpis vseh postaj: ");
+        //testni izpis --> ustrezno branje
+        //System.out.println("izpis vseh postaj: ");
         System.out.println(Stop.preberiPostaje());
 
-        System.out.println("izpis postaj s casi prihodov: ");
-        System.out.print(Stop_times.postajeSCasi());
+        //System.out.println("izpis postaj s casi prihodov: ");
+        //System.out.print(Stop_times.postajeSCasi());
+        Stop_times.postajeSCasi();
 
-        System.out.println("\n izpis tripov: ");
+        //System.out.println("izpis tripov: ");
         System.out.print(Trip.VsiTripi());
 
         //izpis logike
-        // zacni z route-s --> linije
+        // postaja z id 9,
+        System.out.println("izpis prihodov na postajo: \n");
+        Stop_times.prihodiNaPostaji(9, 2, 1);
+
+        //dejanska uporaba iz vnosov:
+        //Stop_times.prihodiNaPostaji(id_postaje, st_avtobusov, nacin_zapisa);
 
     }
 }
--- App/src/vao/Stop_times.java
+++ App/src/vao/Stop_times.java
@@ -5,19 +5,19 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Comparator;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Stop_times {
+    private String trip_id;
     private Stop stop;
     private LocalTime arrival_time;
     private LocalTime departure_time;
     private static ArrayList<Stop_times> AllStops_times = new ArrayList();
 
     //konstruktor
-    public Stop_times(Stop stop, LocalTime arrival_time, LocalTime departure_time){
+    public Stop_times(String trip_id, Stop stop, LocalTime arrival_time, LocalTime departure_time){
+        this.trip_id = trip_id;
         this.stop = stop;
         this.arrival_time = arrival_time;
         this.departure_time = departure_time;
@@ -59,8 +59,16 @@ public class Stop_times {
         AllStops_times = allStops_times;
     }
 
+    public String getTrip_id() {
+        return trip_id;
+    }
+
+    public void setTrip_id(String trip_id) {
+        this.trip_id = trip_id;
+    }
+
     //branje
-    public static ArrayList postajeSCasi() {
+    public static void postajeSCasi() {
         BufferedReader br;
         {
             try {
@@ -81,7 +89,7 @@ public class Stop_times {
                     LocalTime departure = LocalTime.parse(podatki[2]);
 
                     //ustvarim objekt postaje s casom prihoda
-                    Stop_times Stop_wTime = new Stop_times(stop, arrival, departure);
+                    Stop_times Stop_wTime = new Stop_times(podatki[0], stop, arrival, departure);
 
                     //lahko bi zdruzil z ustvarjanjem
                     AllStops_times.add(Stop_wTime);
@@ -92,8 +100,6 @@ public class Stop_times {
                 throw new RuntimeException(e);
             }
         }
-
-        return AllStops_times;
     }
 
     //toString
@@ -103,10 +109,64 @@ public class Stop_times {
     }
 
     //metode
-    public List<Stop_times> sortiranjePoPrihodu(){
-        return AllStops_times.stream().sorted(Comparator.comparing(Stop_times::getArrival_time)).collect(Collectors.toList());
+    public static void prihodiNaPostaji(int stopId, int steviloAvtobusov, int nacin) {
+        LocalTime now = LocalTime.now();
+
+        // 1. Filtriramo stop_times za to postajo in naslednji 2 uri
+        List<Stop_times> prihodi = Stop_times.getAllStops_times().stream()
+                .filter(st -> st.getStop().getStop_id() == stopId)
+                .filter(st -> st.getArrival_time().isAfter(now) && st.getArrival_time().isBefore(now.plusHours(2)))
+                .collect(Collectors.toList());
+
+        System.out.println("filtriran list: " + prihodi);
+
+        // 2. Združimo po route_id da dobimo kljuc vrednost (linija in casi)
+        Map<Integer, List<LocalTime>> poLinijah = new HashMap<>();
+
+        //filtrirajmo pridobljen trip_id iz stop times iz razrednega atributa AllTrips
+        for (Stop_times st : prihodi) {
+            String tripId = st.getTrip_id();
+
+            //preverimo ali v Trip obstaja ta trip id, ki je v Stop_times
+            Optional<Trip> tripOpt = Trip.getAllTrips().stream().filter(trip -> trip.getTrip_id().equals(tripId)).findFirst();
+
+            //ce imamo trip s tem id potem vzamimo route id tega tripa in dodajmo
+            if (tripOpt.isPresent()) {
+                int routeId = tripOpt.get().getRoute_id();
+                //poLinijah.computeIfAbsent(routeId, k -> new ArrayList<>()).add(st.getArrival_time()); //nevem kako dobiti zdruziti kdaj pride avtobus na postajo in route--> ugotovil
+
+                // Preverimo, ali ključ že obstaja v poLinijah
+                if (poLinijah.containsKey(routeId)) {
+                    // Če obstaja, preprosto dodamo čas prihoda v obstoječi seznam
+                    poLinijah.get(routeId).add(st.getArrival_time());
+                } else {
+                    // Če ključ ne obstaja, ustvarimo nov seznam, dodamo čas in ga shranimo v mapo
+                    List<LocalTime> newTimes = new ArrayList<>();
+                    newTimes.add(st.getArrival_time());
+                    poLinijah.put(routeId, newTimes);
+                }
+            }
+        }
+
+        //pogoj za izpis:
+        if(nacin == 1) {
+            // 3. Izpišemo v obliki:
+            // 6: 12:10, 12:15, 12:40
+            for (Map.Entry<Integer, List<LocalTime>> entry : poLinijah.entrySet()) {
+                int routeId = entry.getKey();
+                List<LocalTime> casi = entry.getValue().stream()
+                        .sorted()
+                        .collect(Collectors.toList());
+
+                String ure = casi.stream()
+                        .map(time -> time.toString()) // ali time.format(DateTimeFormatter.ofPattern("HH:mm"))
+                        .collect(Collectors.joining(", "));
+
+                System.out.println(routeId + ": " + ure);
+            }
+        }else if(nacin == 2){
+            //logika za
+
+        }
     }
-
-    //izpis postaj
-
 }
